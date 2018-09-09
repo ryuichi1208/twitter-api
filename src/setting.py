8874af9c8036535c9f85578561c87cd51729a70e
ryuichi1208 <ryucrosskey@gmail.com>
2018-09-09 07:47:13 +0000

    first commit

---
+++ src/setting.py
@@ -0,0 +1,5 @@
+CONSUMER_KEY = "xxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN = "xxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
